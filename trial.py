9ca5781db69cbba4a77a445a726994bb111e2398
fervil3 <fervilmaure@ymail.com>
2022-11-04 10:14:14 +0800

    initial commit

---
+++ trial.py
@@ -0,0 +1,16 @@
+
+studentOne = {
+    "name":"FERVIL",
+    "course": "BSECE",
+    "Age": "27,"
+}
+
+studentTwo = {
+    "name":"jam",
+    "course": "BSIT",
+    "Age": "26,"
+
+}
+
+print(studentOne)
+print(studentTwo)
---
+++ variables.py
@@ -0,0 +1,7 @@
+
+FirstNumber = float(input("Enter First Number : "))
+SecondNumber = float(input("Enter Second Number : "))
+
+result = FirstNumber * SecondNumber
+
+print(str(FirstNumber) + " * " + str(SecondNumber) + " = " + str(result))
